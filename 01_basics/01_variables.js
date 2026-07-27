6f5ee851314d14ca16e93dcf66add5e15c458a4b
Shashank1-Esc <faakkey1357@gmail.com>
2026-07-27 07:46:24 +0000

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+ 
+// accountId = 2
+
+accountEmail = "123@gmail.com"
+accountPassword = "22222"
+accountCity = "assam"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict"; // treat all js code as newer version
+
+// alert("hello") we are using node js not browser
+
+console.log("3 + 3")
+
+console.log('shashank')
+
+let name = "shashank"
+let age = 19
+let isLoggedIn = false
+
+// number => 2^53 -1
+// bigint
+// string =>""
+// bollean => true/false
+// null => standalone value
+// undefined => no variable declared
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); 
