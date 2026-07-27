782391b76eb29efc0c9d1e7e77b485747488fcca
Shashank1-Esc <faakkey1357@gmail.com>
2026-07-27 07:47:59 +0000

    Initial commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+/*
+let score = "shashank"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+*/
+
+// 33 => number
+// "33abc" => NaN not a number
+// "null" => 0
+// undefined => NaN
+// true => 1
+// "33" => 33
+
+/*
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+*/
+
+// 1 => true ; 0 => false
+// "" => false
+// "shashank" => true
+// "0" => true
+// null, undefined => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
