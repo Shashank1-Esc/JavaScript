2d0e1c97be302c8251d17dc9b95c7598071c85a6
Shashank1-Esc <faakkey1357@gmail.com>
2026-08-15 17:39:46 +0000

    loops, forEach, map, filter, reduce

---
+++ 05_iterations/six.js
@@ -0,0 +1,31 @@
+/*
+    forEach and filter both takes the call back function but forEach doesn't return any value whereas 
+    filter return the values store in a variable
+
+*/
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4 // in curly braces scope starts which needs a return to return the values
+// })
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })                                            
+
+// console.log(newNums);
+
